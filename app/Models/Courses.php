ada83a3ea67e1bf5a879b06b2312f0a41afe9f72
danti96 <daniel.tintin86@gmail.com>
2021-01-08 04:39:53 +0000

    Creacion de tablas

---
+++ app/Models/Courses.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Courses extends Model
+{
+    use HasFactory;
+    const BORRADOR = 1;
+    const REVISION = 2;
+    const PUBLICADO = 3;
+}
