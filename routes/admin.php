6572af39d97e8b0e0e0a6dc03e8a99805f3d5d92
danti96 <daniel.tintin86@gmail.com>
2021-02-14 05:49:24 +0000

    29. Agregar permisos y preparar entorno de

--- routes/admin.php
+++ routes/admin.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\Admin\HomeController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Admin\RoleController;
 
-
-Route::get('', [HomeController::class, 'index']);
+Route::get('', [HomeController::class, 'index'])->name('home');
+Route::resource('roles', RoleController::class)->names('roles');
