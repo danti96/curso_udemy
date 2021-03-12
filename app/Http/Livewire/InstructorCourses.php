3d1e148c2fc379464f87784dd3df55240d6c7434
danti96 <daniel.tintin86@gmail.com>
2021-03-12 03:25:39 +0000

    33. Mostrar el listado de cursos de un instructor

--- app/Http/Livewire/InstructorCourses.php
+++ app/Http/Livewire/InstructorCourses.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Course;
 use Livewire\Component;
+
+use Livewire\WithPagination;
 
 class InstructorCourses extends Component
 {
+    use WithPagination;
+    public $search;
+
     public function render()
     {
-        return view('livewire.instructor-courses');
+        $courses = Course::where('title', 'LIKE', '%' . $this->search . '%')
+            ->where('user_id', auth()->user()->id)
+            ->paginate(8);
+
+        return view('livewire.instructor-courses', compact('courses'));
+    }
+
+    public function limpiarPage()
+    {
+        $this->reset('page');
     }
 }
